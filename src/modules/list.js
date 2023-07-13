73460d24c64fc61987d942ced537205bb3ea4c72
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-07-13 06:26:09 +0300

    Add edit function

--- src/modules/list.js
+++ src/modules/list.js
@@ -2,18 +2,45 @@ let tasks = [];
 
 tasks.sort((a, b) => a.index - b.index);
 
+function makeTaskEditable(taskItem, task) {
+  const inputField = document.createElement('input');
+  inputField.type = 'text';
+  inputField.value = task.description;
+  inputField.classList.add('list-li');
+  taskItem.innerHTML = '';
+  taskItem.appendChild(inputField);
+
+  inputField.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+      task.description = inputField.value;
+      saveTasksToLocalStorage();
+      createItem();
+    }
+  });
+
+  inputField.focus();
+}
+
+
 export function createItem() {
   const list = document.getElementById('list');
   list.innerHTML = '';
   tasks.forEach((task) => {
     const aTask = document.createElement('li');
     list.appendChild(aTask);
+    aTask.classList.add('list-li')
+
+    aTask.addEventListener('click', function () {
+      makeTaskEditable(aTask, task);
+    });
+
     aTask.textContent = task.description;
     if (task.completed) {
       aTask.classList.add('completed');
     }
   });
 }
+
 
 export function addTask(description) {
   const newTask = {
